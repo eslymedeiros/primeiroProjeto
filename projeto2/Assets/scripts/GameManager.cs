094a0807748d19355207c2e86c050c3d36b57de7
Esly Medeiros dos Santos <20221170150066@ifrn.local>
2023-11-06 14:29:56 -0300

    projetoFINAL

--- projeto2/Assets/scripts/GameManager.cs
+++ projeto2/Assets/scripts/GameManager.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class msgVitoria : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI hud, msgVitoria;
     public int restantes;
     // Start is called before the first frame update
     void Start()
     {
-        restantes = FindObjectOfType<Moeda>().Length;
+        restantes = FindObjectsOfType<moeda>().Length;
 
-        hud.text = $"Moedas restantes: {restante}";
+        hud.text = $"Moedas restantes: {restantes}";
     }
 
     // Update is called once per frame
-    void Update()
+
+    public void SubtrairMoedas(int valor)
     {
-        msgVitoria.text = "Parabéns!";
+        restantes -= valor;
+        hud.text = $"Moedas restantes: {restantes}";
+
+        if (restantes == 0){
+            msgVitoria.text = "PARABÉNS!";
+        }
     }
 }
--- projeto2/Assets/scripts/moeda.cs
+++ projeto2/Assets/scripts/moeda.cs
@@ -13,6 +13,7 @@ public class moeda : MonoBehaviour
 
     private void OnTriggerEnter(Collider other){
         if (other.tag == "Player"){
+            FindObjectOfType<GameManager>().SubtrairMoedas(1);
             Destroy(gameObject);
         }
     }
