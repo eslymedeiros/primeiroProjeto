ed3065850dd048e3ddea7558675770c48b1ef003
eslymedeiros <esly.medeiros@escolar.ifrn.edu.br>
2023-11-06 19:11:36 -0300

    a

--- projeto2/Assets/scripts/GameManager.cs
+++ projeto2/Assets/scripts/GameManager.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class msgVitoria : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI hud, msgVitoria;
     public int restantes;
     // Start is called before the first frame update
     void Start()
     {
-        restantes = FindObjectOfType<Moeda>().Length;
-
-        hud.text = $"Moedas restantes: {restante}";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        msgVitoria.text = "Parabéns!";
+        
     }
 }
