6f3a02bf70960b037000684e70480f850792999c
eslymedeiros <esly.medeiros@escolar.ifrn.edu.br>
2023-11-08 18:46:06 -0300

    audio

--- projeto2/Assets/scripts/GameManager.cs
+++ projeto2/Assets/scripts/GameManager.cs
@@ -7,15 +7,29 @@ public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI hud, msgVitoria;
     public int restantes;
+    public AudioClip clipMoeda, clipVitoria;
+
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TryGetComponent(out source);
+        restantes = FindObjectsOfType<moeda>().Length;
+        hud.text = $"Moédas restantes: {restantes}";
     }
 
     // Update is called once per frame
-    void Update()
+    public void SubtrairMoedas(int valor)
     {
-        
+        restantes -= valor;
+        hud.text = $"Moédas restantes: {restantes}";
+
+        source.PlayOneShot(clipMoeda);
+
+        if (restantes <= 0){
+            msgVitoria.text = "Parabéns!";
+            source.Stop();
+            source.PlayOneShot(clipVitoria);
+        }
     }
 }
